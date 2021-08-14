a2364127a640164a4bec7f7df59bbb4b1a4ce8f0
Bruno Franco Vilela <brunofvilela1996@gmail.com>
2021-08-13 22:34:03 -0300

    Instalação sqlite

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -7,9 +7,11 @@ export default function Modal(){
 
 
     function open(){
+        //funcionalidade de atribuir a classe active para a modal
         modalWrapper.classList.add("active")
     }
     function close(){
+        //funcionalidade de remover a classe active da modal
         modalWrapper.classList.remove("active")
     }
 
--- src/controllers/QuestionController.js
+++ src/controllers/QuestionController.js
@@ -1,11 +1,11 @@
 module.exports = {
     index(req, res){
         const roomId = req.params.roomId
-        const questionId = req.params.questionId
+        const questionId = req.params.question
         const action = req.params.action
         const password = req.body.password
 
-        console.log(`room = ${roomId}, questionId = ${questionId}
+        console.log(`room = ${roomId}, question = ${questionId}
         action = ${action}, password = ${password}`)
     }
 }
